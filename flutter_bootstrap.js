9d2619d125bd4f02660268f9a1c1709fde2cd146
sixtick_pico <sixtick65@gmail.com>
2025-06-19 13:47:27 +0900

    web app deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3816275527"
+    serviceWorkerVersion: "353858671"
   }
 });
